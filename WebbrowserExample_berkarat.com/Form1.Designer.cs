564bbeda80f4d2de9668cd61e1fb6cd90766cec1
Berk Arat <berkarat93@hotmail.com>
2019-01-13 13:12:38 +0300

    C# Webbrowser Example
    C# Webbrowser kullanımıyla ilgili örnek mevcuttur.
    Detaylı anlatım berkarat.com içerisinde mevcuttur.

---
+++ WebbrowserExample_berkarat.com/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WebbrowserExample_berkarat.com
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.backbtn = new System.Windows.Forms.ToolStripButton();
+            this.forwardbtn = new System.Windows.Forms.ToolStripButton();
+            this.refreshbtn = new System.Windows.Forms.ToolStripButton();
+            this.homebtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1233, 529);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1233, 529);
+            this.panel1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backbtn,
+            this.forwardbtn,
+            this.toolStripSeparator2,
+            this.refreshbtn,
+            this.homebtn,
+            this.toolStripSeparator1,
+            this.toolStripTextBox1,
+            this.toolStripSeparator3,
+            this.toolStripButton1,
+            this.toolStripSeparator4,
+            this.toolStripLabel1,
+            this.toolStripTextBox2,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(1233, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(500, 25);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(150, 25);
+            this.toolStripTextBox2.Click += new System.EventHandler(this.toolStripTextBox2_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Margin = new System.Windows.Forms.Padding(30, 1, 0, 2);
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(42, 22);
+            this.toolStripLabel1.Text = "Search";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Margin = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // backbtn
+            // 
+            this.backbtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.backbtn.Image = global::WebbrowserExample_berkarat.com.Properties.Resources.left_arrow;
+            this.backbtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.backbtn.Name = "backbtn";
+            this.backbtn.Size = new System.Drawing.Size(23, 22);
+            this.backbtn.Text = "toolStripButton1";
+            this.backbtn.Click += new System.EventHandler(this.backbtn_Click);
+            // 
+            // forwardbtn
+            // 
+            this.forwardbtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.forwardbtn.Image = global::WebbrowserExample_berkarat.com.Properties.Resources.right_arrow;
+            this.forwardbtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.forwardbtn.Name = "forwardbtn";
+            this.forwardbtn.Size = new System.Drawing.Size(23, 22);
+            this.forwardbtn.Text = "toolStripButton1";
+            this.forwardbtn.Click += new System.EventHandler(this.forwardbtn_Click);
+            // 
+            // refreshbtn
+            // 
+            this.refreshbtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.refreshbtn.Image = global::WebbrowserExample_berkarat.com.Properties.Resources.refresh;
+            this.refreshbtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.refreshbtn.Name = "refreshbtn";
+            this.refreshbtn.Size = new System.Drawing.Size(23, 22);
+            this.refreshbtn.Text = "toolStripButton1";
+            this.refreshbtn.Click += new System.EventHandler(this.refreshbtn_Click);
+            // 
+            // homebtn
+            // 
+            this.homebtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.homebtn.Image = global::WebbrowserExample_berkarat.com.Properties.Resources.homepage;
+            this.homebtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.homebtn.Name = "homebtn";
+            this.homebtn.Size = new System.Drawing.Size(23, 22);
+            this.homebtn.Text = "toolStripButton1";
+            this.homebtn.Click += new System.EventHandler(this.homebtn_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::WebbrowserExample_berkarat.com.Properties.Resources.send;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Margin = new System.Windows.Forms.Padding(10, 1, 0, 1);
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 23);
+            this.toolStripButton1.Text = "GO";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::WebbrowserExample_berkarat.com.Properties.Resources.magnifying_glass;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.Image = global::WebbrowserExample_berkarat.com.Properties.Resources.folder;
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(105, 22);
+            this.toolStripDropDownButton1.Text = "Favorite Sites";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = global::WebbrowserExample_berkarat.com.Properties.Resources.star;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 529);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton forwardbtn;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripButton backbtn;
+        private System.Windows.Forms.ToolStripButton homebtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton refreshbtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox2;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+    }
+}
+
---
+++ WebbrowserExample_berkarat.com/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebbrowserExample_berkarat.com
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// BERK ARAT
+        /// berkarat.com 
+        /// Web Browser Example
+        /// </summary>
+        #region Declares
+
+        string gidilen_url = string.Empty;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent(); webBrowser1.Navigate("https://www.google.com");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            forwardbtn.Enabled = false;
+            backbtn.Enabled = false;
+
+        }
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {// GO BTN 
+            webBrowser1.Navigate(toolStripTextBox1.Text);
+
+        }
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (toolStripTextBox2.Text.Length > 1)
+            {
+                webBrowser1.Navigate("https://www.google.com/search?q=" + toolStripTextBox2.Text);
+            }
+            else
+            {
+                webBrowser1.Navigate("https://www.google.com");
+            }
+
+        }
+        private void homebtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.google.com");
+        }
+        private void forwardbtn_Click(object sender, EventArgs e)
+        {
+
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+        private void backbtn_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+            }
+
+        }
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            gidilen_url = e.Url.ToString();
+
+            if (webBrowser1.CanGoBack)
+            {
+                backbtn.Enabled = true;
+            }
+            else
+            {
+                backbtn.Enabled = false;
+
+            }
+            if (webBrowser1.CanGoForward)
+            {
+                forwardbtn.Enabled = true;
+            }
+            else
+            {
+                forwardbtn.Enabled = false;
+
+            }
+        }
+        private void toolStripTextBox2_Click(object sender, EventArgs e)
+        {
+            toolStripTextBox2.Clear();
+        }
+        private void refreshbtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(gidilen_url);
+
+        }
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            // favorite kontrolü aynısı varsa eklemez 
+
+            bool isexist =false ;
+            for (int i = 0; i < toolStripDropDownButton1.DropDownItems.Count; i++)
+            {
+                if (toolStripDropDownButton1.DropDownItems[i].Text == gidilen_url)
+                {
+                    isexist = true;
+                    break;
+                }
+
+            }
+            if (!isexist)
+            {
+                toolStripDropDownButton1.DropDownItems.Add(gidilen_url);
+            }
+        }
+    }
+}
